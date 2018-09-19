816b551f9df725fcd00b5a133a3ce81c0953a504
Alex Long <atqlong@gmail.com>
2018-09-19 15:11:46 +0100

    Second_try

---
+++ quiz.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Monday morning HTML Quiz</title>
+  </head>
+  <body>
+
+
+
+    <h1>Section 1</h1>
+    <!-- 1. Create an HTML document with:
+        - title 'Monday morning HTML Quiz'
+        - Page heading: 'Quiz'
+        - paragraph: 'This document is for the Thursday afternoon quiz'
+        - A numbered list with your answers to the remaining questions in sections 1 & 2
+
+    2. What does HTML stand for?
+
+    3. What is the purpose of HTML?
+
+    4. Create a link to: www.example.com, that contains the text 'Click here'. Bonus point if the link opens in a new tab. -->
+
+    <h1>Quiz</h1>
+    <p>This document is for the Thursday afternoon quiz.</p>
+    <ol>
+      <li>What does HTML stand for?
+          Hyper Text Markup Language
+      </li>
+      <li>To give structure to a webpage.</li>
+      <a href="http://example.com/"target="_blank"></a>
+
+
+
+  </body>
+</html>
