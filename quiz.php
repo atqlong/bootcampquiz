547f094896fd50128007798ca17a39beb32e42d5
Alex Long <atqlong@gmail.com>
2018-09-19 15:31:59 +0100

    Section 1+2 Complete

--- quiz.php
+++ quiz.php
@@ -1,36 +1,54 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Monday morning HTML Quiz</title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <title>Monday morning HTML Quiz</title>
+
+  <link rel="stylesheet" href="QuizStyle.css">
+
+</head>
+<body>
 
 
 
-    <h1>Section 1</h1>
-    <!-- 1. Create an HTML document with:
-        - title 'Monday morning HTML Quiz'
-        - Page heading: 'Quiz'
-        - paragraph: 'This document is for the Thursday afternoon quiz'
-        - A numbered list with your answers to the remaining questions in sections 1 & 2
+  <h1>Section 1</h1>
 
-    2. What does HTML stand for?
+  <!-- 1. Create an HTML document with:
+  - title 'Monday morning HTML Quiz'
+  - Page heading: 'Quiz'
+  - paragraph: 'This document is for the Thursday afternoon quiz'
+  - A numbered list with your answers to the remaining questions in sections 1 & 2
 
-    3. What is the purpose of HTML?
+  2. What does HTML stand for?
 
-    4. Create a link to: www.example.com, that contains the text 'Click here'. Bonus point if the link opens in a new tab. -->
+  3. What is the purpose of HTML?
 
-    <h1>Quiz</h1>
-    <p>This document is for the Thursday afternoon quiz.</p>
-    <ol>
-      <li>What does HTML stand for?
-          Hyper Text Markup Language
-      </li>
-      <li>To give structure to a webpage.</li>
-      <a href="http://example.com/"target="_blank"></a>
+  4. Create a link to: www.example.com, that contains the text 'Click here'. Bonus point if the link opens in a new tab. -->
+
+  <!-- Section 2: CSS
+  5. What does it stand for?
+
+  6. What is the purpose of CSS?
+
+  7. Describe 2 ways of appling CSS rules to an HTML element.
+
+  8. Create a CSS rule that causes text to appear red on a blue background for all paragraphs on the page. -->
+
+  <h1>Quiz</h1>
+  <p>This document is for the Thursday afternoon quiz.</p>
+  <ol>
+    <li>What does HTML stand for?
+      Hyper Text Markup Language
+    </li>
+    <li>To give structure to a webpage.</li>
+    <li><a href="http://example.com/"target="_blank">Click to go to Example.com!</a></li>
+    <li>What does it stand for? Custom Style Sheet</li>
+    <li>CSS is the language used for describing the presentation of Web pages to the browser.</li>
+    <li>CSS can be applied internally into the HTML by being directly placed within the header tags within the HTML.
+      CSS can also be applied externally by linking to a seperate CSS file also within the HTML header tags.</li>
+  </ol>
 
 
 
-  </body>
+</body>
 </html>
